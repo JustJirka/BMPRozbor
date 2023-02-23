df8a039575897e8f83f9caff1a835460b5a25a7a
JustJirka <41378892+JustJirka@users.noreply.github.com>
2023-02-23 13:29:55 +0100

    info vypisuje neco

--- BMPRozbor/Form1.cs
+++ BMPRozbor/Form1.cs
@@ -21,21 +21,33 @@ namespace BMPRozbor
 
         private void btn_openFile_Click(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog()!= DialogResult.OK) return;
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
             FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
             var memoryStream = new MemoryStream();
             // Use the .CopyTo() method and write current filestream to memory stream
             fs.CopyTo(memoryStream);
             // Convert Stream To Array
             byte[] byteArray = memoryStream.ToArray();
-            //get bmp header
-            byte[] bmpHeader = new byte[14];
-            Array.Copy(byteArray, 0, bmpHeader, 0, 14);
-            //get bmp info
-            byte[] bmpInfo = new byte[40];
-            Array.Copy(byteArray, 14, bmpInfo, 0, 40);
-            
-            
+            //Get file lenght
+            int delkaSouboru = ByteArrayToWholeValue(SubArray(byteArray,2,4));
+            int zacatekObrazku = ByteArrayToWholeValue(SubArray(byteArray, 10, 4));
+            txtBx_info.Text += "Delka souboru: " + delkaSouboru + " byte"+ Environment.NewLine;
+            txtBx_info.Text += "Zacatek obrazku: " + zacatekObrazku + Environment.NewLine;
+        }
+        public int ByteArrayToWholeValue(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            for (int i = ba.Length-1; i >=0 ; i--)
+            {
+                hex.AppendFormat("{0:x2}", ba[i]);
+            }
+            return int.Parse(hex.ToString(), System.Globalization.NumberStyles.HexNumber);
+        }
+        public T[] SubArray<T>(T[] array, int offset, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(array, offset, result, 0, length);
+            return result;
         }
     }
 }
