7b303b9d4a52f8c70c12a86809cb0d95e6951008
JustJirka <41378892+JustJirka@users.noreply.github.com>
2023-02-23 12:59:54 +0100

    Přidejte soubory projektu.

---
+++ BMPRozbor/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace BMPRozbor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_openFile = new System.Windows.Forms.Button();
+            this.txtBx_info = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_openFile
+            // 
+            this.btn_openFile.Location = new System.Drawing.Point(683, 12);
+            this.btn_openFile.Name = "btn_openFile";
+            this.btn_openFile.Size = new System.Drawing.Size(105, 23);
+            this.btn_openFile.TabIndex = 0;
+            this.btn_openFile.Text = "Otevři soubor";
+            this.btn_openFile.UseVisualStyleBackColor = true;
+            this.btn_openFile.Click += new System.EventHandler(this.btn_openFile_Click);
+            // 
+            // txtBx_info
+            // 
+            this.txtBx_info.Location = new System.Drawing.Point(595, 66);
+            this.txtBx_info.Multiline = true;
+            this.txtBx_info.Name = "txtBx_info";
+            this.txtBx_info.ReadOnly = true;
+            this.txtBx_info.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBx_info.Size = new System.Drawing.Size(193, 356);
+            this.txtBx_info.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBx_info);
+            this.Controls.Add(this.btn_openFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_openFile;
+        private System.Windows.Forms.TextBox txtBx_info;
+    }
+}
+
---
+++ BMPRozbor/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMPRozbor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_openFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()!= DialogResult.OK) return;
+            FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+            var memoryStream = new MemoryStream();
+            // Use the .CopyTo() method and write current filestream to memory stream
+            fs.CopyTo(memoryStream);
+            // Convert Stream To Array
+            byte[] byteArray = memoryStream.ToArray();
+            //get bmp header
+            byte[] bmpHeader = new byte[14];
+            Array.Copy(byteArray, 0, bmpHeader, 0, 14);
+            //get bmp info
+            byte[] bmpInfo = new byte[40];
+            Array.Copy(byteArray, 14, bmpInfo, 0, 40);
+            
+            
+        }
+    }
+}
