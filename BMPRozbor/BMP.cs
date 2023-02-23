dc89cb3c3f02927a307d08a821159d5e24035543
JustJirka <41378892+JustJirka@users.noreply.github.com>
2023-02-23 22:38:20 +0100

    Předělané do tříd

--- BMPRozbor/BMP.cs
+++ BMPRozbor/BMP.cs
@@ -1,13 +1,104 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BMPRozbor
 {
-    internal class BMP
+    class BMP
     {
+        public byte[] byteArray;
+        public BMP(byte[] input)
+        {
+            byteArray = input;
+        }
+        public string BfType()
+        {
+            return Convert.ToString(Convert.ToChar(byteArray[0])) + Convert.ToString(Convert.ToChar(byteArray[1]));//ASCII řetězce "BM"
+        }
+        public int BfSize()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 2, 4)); //Tyto 4 bajty určují celkovou velikost souboru s obrazovými údaji.
+
+        }
+        public int BfOffBits()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 10, 4));//Posun struktury BMP File Header od začátku vlastních obrazových dat
+        }
+        public int BiSize()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 14, 4)); //Tato položka specifikuje celkovou velikost datové struktury Bitmap Information.
+        }
+        public int BiWidth()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 18, 4));  //Tato položka udává šířku obrazu zadávanou v pixelech.
+        }
+        public int BiHeight()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 22, 4)); //Tato položka udává výšku obrazu zadávanou taktéž v pixelech
+
+        }
+        public int BiPlanes()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 26, 2)); //V této položce je zadaný počet bitových rovin pro výstupní zařízení.V BMP, jakožto formátu nezávislého na zařízení, je zde vždy hodnota 1
+        }
+        public int BiBitCount() 
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 28, 2)); //Tato položka udává počet bitů na pixel. V BMP jsou povoleny pouze hodnoty 1, 4, 8, 16, 24 a 32.
+        }
+        public int BiCompression()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 30, 4)); //Tato položka určuje typ komprese použitého pro uložení obrazových dat. V BMP jsou povoleny pouze hodnoty 0, 1, 2 a 3.
+        }
+        public int BiSizeImage() 
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 34, 4)); //Tato položka udává velikost obrazových dat v bajtech. Pokud je hodnota 0, pak je velikost obrazových dat určena způsobem, který je popsán v této stránce.
+        }
+        public int BiXPelsPerMeter()
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 38, 4)); //Udává horizontální rozlišení výstupního zařízení v pixelech na metr. Tato hodnota může být použita například pro výběr obrazu ze skupiny obrazů, který nejlépe odpovídá rozlišení daného výstupního zařízení. Většina aplikací však nemá potřebné informace o výstupním zařízení, a proto do této položky vkládá hodnotu 0.
+        }
+        public int BiYPelsPerMeter() 
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 42, 4)); //dává vertikální rozlišení výstupního zařízení v pixelech na metr.Opět, jako u předchozí položky, zde většina programů zapisuje hodnotu 0.
+        }
+        public int BiClrUsed() 
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 46, 4)); //Udává celkový počet barev, které jsou použité v dané bitmapě.Jestliže je tato hodnota nastavena na nulu, znamená to, že bitmapa používá maximální počet barev.Tento počet lze jednoduše zjistit z položky biBitCount. Nenulová hodnota může být použita například při optimalizacích zobrazování.
+        }
+        public int BiClrImportant() 
+        {
+            return ByteArrayToWholeValue(SubArray(byteArray, 50, 4));//dává počet barev, které jsou důležité pro vykreslení bitmapy.Pokud je tato hodnota nulová, jsou všechny barvy důležité.Tento údaj je používán při zobrazování na zařízeních, které mají omezený počet současně zobrazitelných barev. Ovladač displeje může upravit systémovou paletu tak, aby zobrazil daný obrázek co nejvěrněji.Také je vhodné upravit paletu metodou seřazení jednotlivých barev podle důležitosti
+        }
+        public int ScanlineDoplnek()
+        {
+            decimal nasobek = Convert.ToDecimal(ByteArrayToWholeValue(SubArray(byteArray, 28, 2)) * ByteArrayToWholeValue(SubArray(byteArray, 18, 4)));
+            return Convert.ToInt32((Math.Ceiling( nasobek / 32) * 32 - nasobek));
+        }
+
+
+
+
+        public T[] SubArray<T>(T[] array, int offset, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(array, offset, result, 0, length);
+            return result;
+        }
+
+        public int ByteArrayToWholeValue(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            for (int i = ba.Length - 1; i >= 0; i--)
+            {
+                hex.AppendFormat("{0:x2}", ba[i]);
+            }
+            return int.Parse(hex.ToString(), System.Globalization.NumberStyles.HexNumber);
+        }
 
     }
 }
--- BMPRozbor/Form1.cs
+++ BMPRozbor/Form1.cs
@@ -17,13 +17,9 @@ namespace BMPRozbor
 {
     public partial class Form1 : Form
     {
-        public byte[] byteArray= new byte[0];
+        BMP Soubor;
+        bool imgLoaded = false;
         int imageScale = 8;
-        int biBitCount;
-        int biWidth;
-        int biHeight;
-        int scanlineDoplnek;
-        int bfOffBits;
         public Form1()
         {
             InitializeComponent();
@@ -37,33 +33,16 @@ namespace BMPRozbor
             // Use the .CopyTo() method and write current filestream to memory stream
             fs.CopyTo(memoryStream);
             // Convert Stream To Array
-            byteArray = memoryStream.ToArray();
-            //Get file lenght
-            string bfType = Convert.ToString(Convert.ToChar(byteArray[0])) + Convert.ToString(Convert.ToChar(byteArray[1]));//ASCII řetězce "BM"
-            int bfSize =
-            ByteArrayToWholeValue(SubArray(byteArray, 2, 4)); //Tyto 4 bajty určují celkovou velikost souboru s obrazovými údaji.
-            //reserved 4 
-            bfOffBits = ByteArrayToWholeValue(SubArray(byteArray, 10, 4));//Posun struktury BMP File Header od začátku vlastních obrazových dat
-            int biSize = ByteArrayToWholeValue(SubArray(byteArray, 14, 4)); //Tato položka specifikuje celkovou velikost datové struktury Bitmap Information.
-            biWidth = ByteArrayToWholeValue(SubArray(byteArray, 18, 4));  //Tato položka udává šířku obrazu zadávanou v pixelech.
-            biHeight = ByteArrayToWholeValue(SubArray(byteArray, 22, 4)); //Tato položka udává výšku obrazu zadávanou taktéž v pixelech
-            int biPlanes = ByteArrayToWholeValue(SubArray(byteArray, 26, 2)); //V této položce je zadaný počet bitových rovin pro výstupní zařízení.V BMP, jakožto formátu nezávislého na zařízení, je zde vždy hodnota 1
-            biBitCount = ByteArrayToWholeValue(SubArray(byteArray, 28, 2)); //Tato položka udává počet bitů na pixel. V BMP jsou povoleny pouze hodnoty 1, 4, 8, 16, 24 a 32.
-            int biCompression = ByteArrayToWholeValue(SubArray(byteArray, 30, 4)); //Tato položka určuje typ komprese použitého pro uložení obrazových dat. V BMP jsou povoleny pouze hodnoty 0, 1, 2 a 3.
-            int biSizeImage = ByteArrayToWholeValue(SubArray(byteArray, 34, 4)); //Tato položka udává velikost obrazových dat v bajtech. Pokud je hodnota 0, pak je velikost obrazových dat určena způsobem, který je popsán v této stránce.
-            int biXPelsPerMeter = ByteArrayToWholeValue(SubArray(byteArray, 38, 4)); //Udává horizontální rozlišení výstupního zařízení v pixelech na metr. Tato hodnota může být použita například pro výběr obrazu ze skupiny obrazů, který nejlépe odpovídá rozlišení daného výstupního zařízení. Většina aplikací však nemá potřebné informace o výstupním zařízení, a proto do této položky vkládá hodnotu 0.
-            int biYPelsPerMeter = ByteArrayToWholeValue(SubArray(byteArray, 42, 4)); //dává vertikální rozlišení výstupního zařízení v pixelech na metr.Opět, jako u předchozí položky, zde většina programů zapisuje hodnotu 0.
-            int biClrUsed = ByteArrayToWholeValue(SubArray(byteArray, 46, 4)); //Udává celkový počet barev, které jsou použité v dané bitmapě.Jestliže je tato hodnota nastavena na nulu, znamená to, že bitmapa používá maximální počet barev.Tento počet lze jednoduše zjistit z položky biBitCount. Nenulová hodnota může být použita například při optimalizacích zobrazování.
-            int biClrImportant = ByteArrayToWholeValue(SubArray(byteArray, 50, 4));//dává počet barev, které jsou důležité pro vykreslení bitmapy.Pokud je tato hodnota nulová, jsou všechny barvy důležité.Tento údaj je používán při zobrazování na zařízeních, které mají omezený počet současně zobrazitelných barev. Ovladač displeje může upravit systémovou paletu tak, aby zobrazil daný obrázek co nejvěrněji.Také je vhodné upravit paletu metodou seřazení jednotlivých barev podle důležitosti
-            scanlineDoplnek = Convert.ToInt32((Math.Ceiling(Convert.ToDecimal(biBitCount * biWidth) / 32)*32 -Convert.ToDecimal(biBitCount * biWidth)));
-            txtBx_info.Text += "bfType: " + bfType + Environment.NewLine + "bfSize: " + bfSize + Environment.NewLine + "bfOffBits: " + bfOffBits + Environment.NewLine + "biSize: " + biSize + Environment.NewLine + "biWidth: " + biWidth + Environment.NewLine + "biHeight: " + biHeight + Environment.NewLine + "biPlanes: " + biPlanes + Environment.NewLine + "biBitCount: " + biBitCount + Environment.NewLine + "biCompression: " + biCompression + Environment.NewLine + "biSizeImage: " + biSizeImage + Environment.NewLine + "biXPelsPerMeter: " + biXPelsPerMeter + Environment.NewLine + "biYPelsPerMeter: " + biYPelsPerMeter + Environment.NewLine + "biClrUsed: " + biClrUsed + Environment.NewLine + "biClrImportant: " + biClrImportant;
+            Soubor = new BMP(memoryStream.ToArray());
+            imgLoaded= true;
+            txtBx_info.Text += "bfType: " + Soubor.BfType() + Environment.NewLine + "bfSize: " + Soubor.BfSize() + Environment.NewLine + "bfOffBits: " + Soubor.BfOffBits() + Environment.NewLine + "biSize: " + Soubor.BiSize() + Environment.NewLine + "biWidth: " + Soubor.BiWidth() + Environment.NewLine + "biHeight: " + Soubor.BiHeight() + Environment.NewLine + "biPlanes: " + Soubor.BiPlanes() + Environment.NewLine + "biBitCount: " + Soubor.BiBitCount() + Environment.NewLine + "biCompression: " + Soubor.BiCompression() + Environment.NewLine + "biSizeImage: " + Soubor.BiSizeImage() + Environment.NewLine + "biXPelsPerMeter: " + Soubor.BiXPelsPerMeter() + Environment.NewLine + "biYPelsPerMeter: " + Soubor.BiYPelsPerMeter() + Environment.NewLine + "biClrUsed: " + Soubor.BiClrUsed() + Environment.NewLine + "biClrImportant: " + Soubor.BiClrImportant();
             picBx_hlavni.Refresh();
         }
         public string IntToBinary(int vstup)
         {
             string vystup = "";
             //convert int to binary
-            for (int i = 128; i >0 ; i=i/2)
+            for (int i = 128; i > 0; i /= 2)
             {
                 if (vstup-i>=0)
                 {
@@ -83,34 +62,24 @@ namespace BMPRozbor
             hex.AppendFormat("{0:x2}", i);
             return hex.ToString();
         }
-        public int ByteArrayToWholeValue(byte[] ba)
-        {
-            StringBuilder hex = new StringBuilder(ba.Length * 2);
-            for (int i = ba.Length-1; i >=0 ; i--)
-            {
-                hex.AppendFormat("{0:x2}", ba[i]);
-            }
-            return int.Parse(hex.ToString(), System.Globalization.NumberStyles.HexNumber);
-        }
-        public T[] SubArray<T>(T[] array, int offset, int length)
-        {
-            T[] result = new T[length];
-            Array.Copy(array, offset, result, 0, length);
-            return result;
-        }
 
         private void picBx_hlavni_Paint(object sender, PaintEventArgs e)
         {
-            int curentByte = bfOffBits;
-            for (int i = biHeight; i > 0; i--)
+            if(imgLoaded)
             {
-                for (int j = 0; j < biWidth; j++)
+                int curentByte = Soubor.BfOffBits();
+                for (int i = Soubor.BiHeight(); i > 0; i--)
                 {
-                    e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(byteArray[curentByte + 2], byteArray[curentByte + 1], byteArray[curentByte])), j* imageScale, i* imageScale, imageScale, imageScale);
-                    curentByte +=3;
+                    for (int j = 0; j < Soubor.BiWidth(); j++)
+                    {
+                        e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(Soubor.byteArray[curentByte + 2], Soubor.byteArray[curentByte + 1], Soubor.byteArray[curentByte])), j* imageScale, i* imageScale, imageScale, imageScale);
+                        curentByte +=3;
+                    }
+                    curentByte += Soubor.ScanlineDoplnek();
                 }
-                curentByte += scanlineDoplnek;
             }
+
+
             /*int pocetPalet = Convert.ToInt32(Math.Pow(2, biBitCount));
             Brush[] paleta = new Brush[pocetPalet];
             for (int i = 0; i < pocetPalet; i++)
