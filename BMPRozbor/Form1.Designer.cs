fbad1aefeed5ad0608a6b9c190afb3ad49249727
JustJirka <41378892+JustJirka@users.noreply.github.com>
2023-02-24 17:50:03 +0100

    vykresluje vse

--- BMPRozbor/Form1.Designer.cs
+++ BMPRozbor/Form1.Designer.cs
@@ -32,7 +32,10 @@
             this.btn_openFile = new System.Windows.Forms.Button();
             this.txtBx_info = new System.Windows.Forms.TextBox();
             this.picBx_hlavni = new System.Windows.Forms.PictureBox();
+            this.nuUpDo_imageScale = new System.Windows.Forms.NumericUpDown();
+            this.btn_refresh = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBx_hlavni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuUpDo_imageScale)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -41,7 +44,8 @@
             // 
             // btn_openFile
             // 
-            this.btn_openFile.Location = new System.Drawing.Point(683, 12);
+            this.btn_openFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_openFile.Location = new System.Drawing.Point(1165, 12);
             this.btn_openFile.Name = "btn_openFile";
             this.btn_openFile.Size = new System.Drawing.Size(105, 23);
             this.btn_openFile.TabIndex = 0;
@@ -51,7 +55,8 @@
             // 
             // txtBx_info
             // 
-            this.txtBx_info.Location = new System.Drawing.Point(595, 66);
+            this.txtBx_info.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBx_info.Location = new System.Drawing.Point(1077, 66);
             this.txtBx_info.Multiline = true;
             this.txtBx_info.Name = "txtBx_info";
             this.txtBx_info.ReadOnly = true;
@@ -61,24 +66,64 @@
             // 
             // picBx_hlavni
             // 
+            this.picBx_hlavni.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.picBx_hlavni.Location = new System.Drawing.Point(39, 30);
             this.picBx_hlavni.Name = "picBx_hlavni";
-            this.picBx_hlavni.Size = new System.Drawing.Size(550, 408);
+            this.picBx_hlavni.Size = new System.Drawing.Size(1032, 539);
             this.picBx_hlavni.TabIndex = 2;
             this.picBx_hlavni.TabStop = false;
             this.picBx_hlavni.Paint += new System.Windows.Forms.PaintEventHandler(this.picBx_hlavni_Paint);
+            // 
+            // nuUpDo_imageScale
+            // 
+            this.nuUpDo_imageScale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nuUpDo_imageScale.Location = new System.Drawing.Point(1077, 41);
+            this.nuUpDo_imageScale.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.nuUpDo_imageScale.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuUpDo_imageScale.Name = "nuUpDo_imageScale";
+            this.nuUpDo_imageScale.Size = new System.Drawing.Size(193, 20);
+            this.nuUpDo_imageScale.TabIndex = 3;
+            this.nuUpDo_imageScale.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_refresh.Location = new System.Drawing.Point(1077, 12);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(82, 23);
+            this.btn_refresh.TabIndex = 4;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1282, 672);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.nuUpDo_imageScale);
             this.Controls.Add(this.picBx_hlavni);
             this.Controls.Add(this.txtBx_info);
             this.Controls.Add(this.btn_openFile);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.picBx_hlavni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuUpDo_imageScale)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +135,8 @@
         private System.Windows.Forms.Button btn_openFile;
         private System.Windows.Forms.TextBox txtBx_info;
         private System.Windows.Forms.PictureBox picBx_hlavni;
+        private System.Windows.Forms.NumericUpDown nuUpDo_imageScale;
+        private System.Windows.Forms.Button btn_refresh;
     }
 }
 
--- BMPRozbor/Form1.cs
+++ BMPRozbor/Form1.cs
@@ -19,7 +19,6 @@ namespace BMPRozbor
     {
         BMP Soubor;
         bool imgLoaded = false;
-        int imageScale = 8;
         public Form1()
         {
             InitializeComponent();
@@ -49,6 +48,7 @@ namespace BMPRozbor
         {
             if (imgLoaded)
             {
+                int imageScale = (int)nuUpDo_imageScale.Value;
                 if (Soubor.BiBitCount() == 24)
                 {
                     int curentByte = Soubor.BfOffBits();
@@ -59,39 +59,106 @@ namespace BMPRozbor
                             e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(Soubor.byteArray[curentByte + 2], Soubor.byteArray[curentByte + 1], Soubor.byteArray[curentByte])), j * imageScale, i * imageScale, imageScale, imageScale);
                             curentByte += 3;
                         }
-                        // curentByte += Soubor.ScanlineDoplnek();
+                         curentByte += Soubor.ScanlineDoplnek();
                     }
                 }
-                else if (Soubor.BiBitCount() == 1)
+                else if (Soubor.BiBitCount() == 1 || Soubor.BiBitCount() == 4 || Soubor.BiBitCount() == 8)
                 {
                     int curentByte = Soubor.BfOffBits();
-                    Brush[] paleta = new Brush[2];
-                    for (int i = 0; i < 2; i++)
+                    int pocetPalet = (int)Math.Pow(2, Soubor.BiBitCount());
+                    Brush[] paleta = new Brush[pocetPalet];
+                    for (int i = 0; i < pocetPalet; i++)
                     {
-                        paleta[i] = new SolidBrush(Color.FromArgb(Soubor.byteArray[Soubor.BfOffBits() - 8 + (i * 4) + 2], Soubor.byteArray[Soubor.BfOffBits() - 8 + (i * 4) + 1], Soubor.byteArray[Soubor.BfOffBits() - 8 + (i * 4)]));
+                        paleta[i] = new SolidBrush(Color.FromArgb(Soubor.byteArray[Soubor.BfOffBits() - pocetPalet * 4 + (i * 4) + 2], Soubor.byteArray[Soubor.BfOffBits() - pocetPalet * 4 + (i * 4) + 1], Soubor.byteArray[Soubor.BfOffBits() - pocetPalet * 4 + (i * 4)]));
                     }
-                    for (int i = Soubor.BiHeight(); i > 0; i--)
+                    if (Soubor.BiBitCount() == 1)
                     {
-                        for (int j = 0; j < Soubor.BiWidth();)
+                        for (int i = Soubor.BiHeight(); i > 0; i--)
                         {
-                            string hodnoty = Soubor.IntToBinary(Soubor.byteArray[curentByte]);
-                            for (int k = 0; k < 8; k++)
+                            for (int j = 0; j < Soubor.BiWidth();)
                             {
-                                int indexPalety = (int)(hodnoty[k]) - 48;
-                                e.Graphics.FillRectangle(paleta[indexPalety], j * imageScale, i * imageScale, imageScale, imageScale);
-                                j++;
-                                if (j > Soubor.BiWidth()-1)
+                                string hodnoty = Soubor.IntToBinary(Soubor.byteArray[curentByte]);
+                                for (int k = 0; k < 8; k++)
                                 {
-                                    curentByte += Soubor.ScanlineDoplnek()/8;
-                                    break;
+                                    int indexPalety = (int)(hodnoty[k]) - 48;
+                                    e.Graphics.FillRectangle(paleta[indexPalety], j * imageScale, i * imageScale, imageScale, imageScale);
+                                    j++;
+                                    if (j > Soubor.BiWidth() - 1)
+                                    {
+                                        curentByte += Soubor.ScanlineDoplnek() / 8;
+                                        break;
+                                    }
                                 }
+                                curentByte++;
                             }
-                            curentByte++;
-
                         }
                     }
+                    else if (Soubor.BiBitCount() == 2)
+                    {
+                        for (int i = Soubor.BiHeight(); i > 0; i--)
+                        {
+                            for (int j = 0; j < Soubor.BiWidth();)
+                            {
+                                string hodnoty = Soubor.IntToBinary(Soubor.byteArray[curentByte]);
+                                for (int k = 0; k < 8; k+=2)
+                                {
+
+                                    int indexPalety = ((int)(hodnoty[k]) - 48)*2 + ((int)(hodnoty[k+1]) - 48);
+                                    e.Graphics.FillRectangle(paleta[indexPalety], j * imageScale, i * imageScale, imageScale, imageScale);
+                                    j++;
+                                    if (j > Soubor.BiWidth() - 1)
+                                    {
+                                        curentByte += Soubor.ScanlineDoplnek() / 4;
+                                        break;
+                                    }
+                                }
+                                curentByte++;
+                            }
+                        }
+                    }
+                    else if (Soubor.BiBitCount() == 4)
+                    {
+                        for (int i = Soubor.BiHeight(); i > 0; i--)
+                        {
+                            for (int j = 0; j < Soubor.BiWidth();)
+                            {
+                                string hodnoty = Soubor.IntToBinary(Soubor.byteArray[curentByte]);
+                                for (int k = 0; k < 8; k += 4)
+                                {
+
+                                    int indexPalety = ((int)(hodnoty[k]) - 48) * 8 + ((int)(hodnoty[k+1]) - 48) * 4 + ((int)(hodnoty[k+2]) - 48) * 2 + ((int)(hodnoty[k + 3]) - 48);
+                                    e.Graphics.FillRectangle(paleta[indexPalety], j * imageScale, i * imageScale, imageScale, imageScale);
+                                    j++;
+                                    if (j > Soubor.BiWidth() - 1)
+                                    {
+                                        curentByte += Soubor.ScanlineDoplnek() / 8;
+                                        break;
+                                    }
+                                }
+                                curentByte++;
+                            }
+                        }
+                    }
+                    else if (Soubor.BiBitCount() == 8)
+                    {
+                        for (int i = Soubor.BiHeight(); i > 0; i--)
+                        {
+                            for (int j = 0; j < Soubor.BiWidth(); j++)
+                            {
+                                e.Graphics.FillRectangle(paleta[Soubor.byteArray[curentByte]], j * imageScale, i * imageScale, imageScale, imageScale);
+                                curentByte++;
+                            }
+                            curentByte += Soubor.ScanlineDoplnek()/8;
+                        }
+                    }
+
                 }
             }
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            picBx_hlavni.Refresh();
         }
     }
 }
